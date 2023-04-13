46a0bdb328aa84e32cf076c0dcecad871878e8d7
seanlin1125 <sean821125@gmail.com>
2023-04-13 17:30:34 +0800

    add create user controller and index router

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from 'express'
+
+import { getUserByEmail, createUser } from 'db/users'
+import { random, authentication } from 'helpers'
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body
+    if (!username || !email || !password) {
+      return res.sendStatus(400)
+    }
+    const existingUser = await getUserByEmail(email)
+    if (existingUser) {
+      return res.sendStatus(400)
+    }
+    const salt = random()
+    const user = await createUser({
+      email,
+      username,
+      password: authentication(salt, password),
+    })
+    return res.status(200).json(user).end()
+  } catch (error) {
+    console.log(error)
+    return res.sendStatus(400)
+  }
+}

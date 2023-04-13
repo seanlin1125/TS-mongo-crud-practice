5e85d8b55bf73327191c77e3095bebcc0e24de20
seanlin1125 <sean821125@gmail.com>
2023-04-13 16:55:16 +0800

    add api methods

--- src/db/users.ts
+++ src/db/users.ts
@@ -14,4 +14,14 @@ export const UserModel = mongoose.model('User', UserSchema)
 
 // api method
 
-
+export const getUsers = () => UserModel.find()
+export const getUserByEmail = (email: string) => UserModel.findOne({ email })
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({ 'authentication.sessionToken': sessionToken })
+export const getUserById = (id: string) => UserModel.findById(id)
+export const createUser = (values: Record<string, any>) =>
+  new UserModel(values).save().then((user) => user.toJSON())
+export const deleteUserById = (id: string) =>
+  UserModel.findOneAndDelete({ _id: id })
+export const updateUserById = (id: string, values: Record<string, any>) =>
+  UserModel.findByIdAndUpdate(id, values)
